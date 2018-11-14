6dbf5bfedd2aebb54257860ca3135f548b973562
t0shifollow <marcindymek24@gmail.com>
2018-11-14 22:40:31 +0100

    add toggleTodoDone

--- src/App.js
+++ src/App.js
@@ -2,10 +2,59 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: 'Hello Gays!',
+      newTodo: '',
+      todos: []
+    };
+  }
+
+  newTodoChanged(event) {
+    this.setState({
+      newTodo: event.target.value
+    });
+  }
+
+  formSubmitted(event) {
+    // eslint-disable-next-line no-restricted-globals
+    event.preventDefault();
+    this.setState({
+      newTodo: '',
+      todos: [...this.state.todos, {
+        title: this.state.newTodo,
+        done: false
+      }]
+    });
+  }
+
+  toggleTodoDone(event, index) {
+    console.log("event.target.checked");
+    const todos = [...this.state.todos];
+    const todo = {...todos[index]};
+    todo.done = event.target.checked;
+    this.setState({
+      todos
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hello! </h1>
+        <h3>{this.state.message}</h3>
+        <form onSubmit={(event) => this.formSubmitted(event)}>
+          <label htmlFor="newTodo">New Todo</label>
+          <input onChange={(event) => this.newTodoChanged(event)} id="newTodo" name="newTodo" value={this.state.newTodo} />
+          <button type="submit">Add Todo</button>
+        </form>
+        <ul>
+          {this.state.todos.map((todo, index) => {
+            return (<li key={todo.title}>
+              <input onChange={(event) => this.toggleTodoDone(event, index)} type="checkbox"/>{todo.title}
+              </li>)
+          })}
+        </ul>
       </div>
     );
   }
